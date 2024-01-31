1d50964e4ed5bdc3dd4a7fe107de6426c3dbfcdf
sainrohit98 <rohitsain.rs98@gmail.com>
2024-02-01 00:40:04 +0530

    Initial commit

--- email_campaign/apps.py
+++ email_campaign/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class EmailCampaignConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'email_campaign'
--- email_campaign/tasks.py
+++ email_campaign/tasks.py
@@ -1,11 +1,3 @@
-from celery import shared_task
-from django.core.management import call_command
-
-
-# @shared_task(bind=True)
-# def run_campaigns_daily(self):
-#     call_command('create_campaigns')
-
 
 # campaign_manager/tasks.py
 import requests
